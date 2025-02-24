eb5a9546a0c0982749c15889274a90285ee946e4
Sergey <seygorin@gmail.com>
2025-02-24 20:18:07 +0500

    refactor: optimize code reusability

--- custom-lodash/index.js
+++ custom-lodash/index.js
@@ -15,6 +15,42 @@ const isString = (value) => typeof value === 'string'
 
 const isNaN = (value) => value !== value
 
+const createPredicateFunction = (predicate) =>
+  isFunction(predicate)
+    ? predicate
+    : isObject(predicate) && !isArray(predicate)
+    ? (item) => {
+        for (const key in predicate) {
+          if (item[key] !== predicate[key]) return false
+        }
+        return true
+      }
+    : isArray(predicate) && predicate.length === 2
+    ? (item) => item[predicate[0]] === predicate[1]
+    : isString(predicate)
+    ? (item) => item[predicate]
+    : () => false
+
+const processCollection = (collection, defaultValue, processor) =>
+  collection == null
+    ? defaultValue
+    : isArray(collection)
+    ? processor(collection, [])
+    : isObject(collection)
+    ? processor(collection, {})
+    : defaultValue
+
+const createObjectOperation = (object, condition, defaultValue = {}) =>
+  !isObject(object)
+    ? defaultValue
+    : (() => {
+        const result = {}
+        for (const key in object) {
+          condition(key, object[key]) ? (result[key] = object[key]) : null
+        }
+        return result
+      })()
+
 const chunk = (array, size = 1) => {
   if (!isArray(array)) return []
   if (size < 1) return []
@@ -58,20 +94,7 @@ const drop = (array, n = 1) => {
 const dropWhile = (array, predicate) => {
   if (!isArray(array)) return []
 
-  const predicateFunc = isFunction(predicate)
-    ? predicate
-    : isObject(predicate)
-    ? (item) => {
-        for (const key in predicate) {
-          if (item[key] !== predicate[key]) return false
-        }
-        return true
-      }
-    : isArray(predicate)
-    ? (item) => item[predicate[0]] === predicate[1]
-    : isString(predicate)
-    ? (item) => item[predicate]
-    : () => false
+  const predicateFunc = createPredicateFunction(predicate)
 
   let dropIndex = 0
   let i = 0
@@ -113,56 +136,31 @@ const take = (array, n = 1) => {
 }
 
 const filter = (collection, predicate) =>
-  collection == null || (!isArray(collection) && !isObject(collection))
-    ? []
-    : (() => {
-        const result = []
+  processCollection(collection, [], (coll) => {
+    const result = []
+    const predicateFunc = createPredicateFunction(predicate)
 
-        const predicateFunc = isFunction(predicate)
-          ? predicate
-          : isObject(predicate) && !isArray(predicate)
-          ? (item) => {
-              for (const key in predicate) {
-                if (item[key] !== predicate[key]) return false
-              }
-              return true
-            }
-          : isArray(predicate) && predicate.length === 2
-          ? (item) => item[predicate[0]] === predicate[1]
-          : isString(predicate)
-          ? (item) => !!item[predicate]
-          : () => false
+    if (isArray(coll)) {
+      let i = 0
+      for (const item of coll) {
+        predicateFunc(item, i, coll) ? (result[result.length] = item) : null
+        i++
+      }
+    } else if (isObject(coll)) {
+      for (const key in coll) {
+        predicateFunc(coll[key], key)
+          ? (result[result.length] = coll[key])
+          : null
+      }
+    }
 
-        if (isArray(collection)) {
-          let i = 0
-          for (const item of collection) {
-            if (predicateFunc(item, i, collection)) {
-              result[result.length] = item
-            }
-            i++
-          }
-        }
-
-        return result
-      })()
+    return result
+  })
 
 const find = (array, predicate) => {
   if (!isArray(array)) return false
 
-  const predicateFunc = isFunction(predicate)
-    ? predicate
-    : isObject(predicate)
-    ? (item) => {
-        for (const key in predicate) {
-          if (item[key] !== predicate[key]) return false
-        }
-        return true
-      }
-    : isArray(predicate) && predicate.length === 2
-    ? (item) => item[predicate[0]] === predicate[1]
-    : isString(predicate)
-    ? (item) => item[predicate]
-    : () => false
+  const predicateFunc = createPredicateFunction(predicate)
 
   let i = 0
   for (const item of array) {
@@ -215,58 +213,57 @@ const includes = (collection, value, fromIndex = 0) => {
     : false
 }
 
-const map = (collection, iteratee) => {
-  if (collection == null) return []
-
-  const result = []
-
-  const func = isFunction(iteratee)
-    ? iteratee
-    : isString(iteratee)
-    ? (item) => item[iteratee]
-    : (item) => item
-
-  return isArray(collection)
-    ? (() => {
-        let i = 0
-        for (const item of collection) {
-          result[result.length] = func(item, i, collection)
-          i++
-        }
-        return result
-      })()
-    : isObject(collection)
-    ? (() => {
-        for (const key in collection) {
-          result[result.length] = func(collection[key], key, collection)
-        }
-        return result
-      })()
-    : []
-}
-
-const zip = (...arrays) =>
-  !arrays.length
+const map = (collection, iteratee) =>
+  collection == null
     ? []
     : (() => {
-        let maxLength = 0
-        for (const arr of arrays) {
-          if (!isArray(arr)) return []
-          maxLength = arr.length > maxLength ? arr.length : maxLength
-        }
+        const func = isFunction(iteratee)
+          ? iteratee
+          : isString(iteratee)
+          ? (item) => item[iteratee]
+          : (item) => item
 
-        const result = []
-
-        for (let i = 0; i < maxLength; i++) {
-          const group = []
-          for (const arr of arrays) {
-            group[group.length] = arr[i]
-          }
-          result[result.length] = group
-        }
-
-        return result
+        return isArray(collection)
+          ? (() => {
+              const result = []
+              let i = 0
+              for (const item of collection) {
+                result[result.length] = func(item, i, collection)
+                i++
+              }
+              return result
+            })()
+          : isObject(collection)
+          ? (() => {
+              const result = []
+              for (const key in collection) {
+                result[result.length] = func(collection[key], key, collection)
+              }
+              return result
+            })()
+          : []
       })()
+
+const zip = (...arrays) =>
+  processCollection(arrays, [], (arrs) => {
+    if (!arrs.length) return []
+
+    let maxLength = 0
+    for (const arr of arrs) {
+      if (!isArray(arr)) return []
+      maxLength = arr.length > maxLength ? arr.length : maxLength
+    }
+
+    const result = []
+    for (let i = 0; i < maxLength; i++) {
+      const group = []
+      for (const arr of arrs) {
+        group[group.length] = arr[i]
+      }
+      result[result.length] = group
+    }
+    return result
+  })
 
 const merge = (object, ...sources) =>
   !isObject(object)
@@ -281,6 +278,7 @@ const merge = (object, ...sources) =>
             ? merge(object[key])
             : object[key]
         }
+
         for (const source of sources) {
           if (!isObject(source)) continue
 
@@ -305,56 +303,16 @@ const merge = (object, ...sources) =>
       })()
 
 const omit = (object, paths) =>
-  !isObject(object) || !isArray(paths)
-    ? {}
-    : (() => {
-        const result = {}
-
-        for (const key in object) {
-          !includes(paths, key) ? (result[key] = object[key]) : null
-        }
-
-        return result
-      })()
+  createObjectOperation(object, (key) => !includes(paths, key), {})
 
 const omitBy = (object, predicate) =>
-  !isObject(object) || !isFunction(predicate)
-    ? {}
-    : (() => {
-        const result = {}
-
-        for (const key in object) {
-          !predicate(object[key], key) ? (result[key] = object[key]) : null
-        }
-
-        return result
-      })()
+  createObjectOperation(object, (key, value) => !predicate(value, key), {})
 
 const pick = (object, paths) =>
-  !isObject(object) || !isArray(paths)
-    ? {}
-    : (() => {
-        const result = {}
-
-        for (const path of paths) {
-          path in object ? (result[path] = object[path]) : null
-        }
-
-        return result
-      })()
+  createObjectOperation(object, (key) => includes(paths, key), {})
 
 const pickBy = (object, predicate) =>
-  !isObject(object) || !isFunction(predicate)
-    ? {}
-    : (() => {
-        const result = {}
-
-        for (const key in object) {
-          predicate(object[key], key) ? (result[key] = object[key]) : null
-        }
-
-        return result
-      })()
+  createObjectOperation(object, (key, value) => predicate(value, key), {})
 
 const toPairs = (object) =>
   !isObject(object)
@@ -362,7 +320,7 @@ const toPairs = (object) =>
     : (() => {
         const result = []
         for (const key in object) {
-          Object.getOwnPropertyDescriptor(object, key) && 
+          Object.getOwnPropertyDescriptor(object, key) &&
             (result[result.length] = [key, object[key]])
         }
         return result
