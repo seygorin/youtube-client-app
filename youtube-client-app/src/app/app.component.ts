c509c87100f8921b72a8e0a5d4c3b7065c33c145
Sergey <seygorin@gmail.com>
2025-04-03 15:11:22 +0500

    fix: delete redundant code

--- youtube-client-app/src/app/app.component.ts
+++ youtube-client-app/src/app/app.component.ts
@@ -1,25 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import ThemeToggleComponent from './theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule,
-    ThemeToggleComponent,
-  ],
+  imports: [RouterOutlet, ThemeToggleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export default class AppComponent {
   title = 'youtube-client-app';
-
-  yes: any = 'yes'; // error
 }
--- youtube-client-app/src/app/app.routes.ts
+++ youtube-client-app/src/app/app.routes.ts
@@ -2,14 +2,6 @@ import { Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadComponent: () => import('./home/home.component'),
-  },
-  {
-    path: 'card-details',
-    loadComponent: () => import('./card-details/card-details.component'),
-  },
-  {
     path: '**',
     redirectTo: '',
   },
