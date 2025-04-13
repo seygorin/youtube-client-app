7c867773636bdc533ec92dcba4ae75581b6f3dcb
Sergey <seygorin@gmail.com>
2025-04-13 15:06:19 +0500

    feat: add favorites-list components with logic

--- youtube-client-app/src/app/features/favorites/pages/favorites-list/favorites-list.component.ts
+++ youtube-client-app/src/app/features/favorites/pages/favorites-list/favorites-list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { VideoItem } from '../../../video/models/video.model';
+import { SearchItemComponent } from '../../../video/components/search-item/search-item.component';
+import { selectAllFavorites } from '../../../video/store/favorites/favorites.selectors';
+import { FavoritesActions } from '../../../video/store/favorites/favorites.actions';
 
 @Component({
   selector: 'app-favorites-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, SearchItemComponent],
   templateUrl: './favorites-list.component.html',
-  styleUrl: './favorites-list.component.scss'
+  styleUrls: ['./favorites-list.component.scss'],
 })
-export class FavoritesListComponent {
+export class FavoritesListComponent implements OnInit {
+  favoriteVideos$: Observable<VideoItem[]>;
 
+  constructor(private store: Store) {
+    this.favoriteVideos$ = this.store.select(selectAllFavorites);
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(FavoritesActions.loadFavorites());
+  }
 }
